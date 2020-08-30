cae76868cbcc8d2c3b79cfa934330cf59b83b97d
jpeter88 <jrpeterson@unomaha.edu>
2020-08-30 14:33:46 -0400

    adding updated serverless, lambda, and auth fixes

--- c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,45 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
 import 'source-map-support/register'
+import * as uuid from 'uuid'
+import { parseUserId } from '../../auth/utils'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+const docClient = new AWS.DynamoDB.DocumentClient()
 
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    
+    console.log("EVENT:", event);
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+    const todoId = uuid.v4()
+
+    const parsedBody = JSON.parse(event.body)
+
+    const authHeader = event.headers.Authorization
+    const authSplit = authHeader.split(" ")
+    const token = authSplit[1]
+
+    console.log("test",token)
+
+    const item = {
+      todoId: todoId,
+        userId: parseUserId(token),
+        ...parsedBody
+    }
+
+    await docClient.put({
+        TableName: todosTable,
+        Item: item
+    }).promise()
+
+    return {
+        statusCode: 201,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          item
+        })
+    }
 }
--- c4-final-project-starter-code/backend/src/requests/CreateTodoRequest.ts
+++ c4-final-project-starter-code/backend/src/requests/CreateTodoRequest.ts
@@ -2,6 +2,10 @@
  * Fields in a request to create a single TODO item.
  */
 export interface CreateTodoRequest {
+  createdAt: string
   name: string
   dueDate: string
+  done: boolean
+  attachmentUrl: string
+
 }
